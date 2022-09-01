e9fe5205e0750a391a0aacbcac3e47ef273733d7
Luiz Fernando Severnini <luiz.severnini@embrapa.br>
2022-09-01 10:34:48 -0300

    Implement loading existing sqlite from assets

--- App.js
+++ App.js
@@ -1,13 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as FileSystem from 'expo-file-system'
+import * as SQLite from 'expo-sqlite'
+
+import { StyleSheet, Text, View } from 'react-native'
+import { useEffect, useState } from 'react'
+
+import { Asset } from 'expo-asset'
+import { StatusBar } from 'expo-status-bar'
+
+async function openDatabase() {
+  if (!(await FileSystem.getInfoAsync(FileSystem.documentDirectory + 'SQLite')).exists) {
+    await FileSystem.makeDirectoryAsync(FileSystem.documentDirectory + 'SQLite')
+  }
+  await FileSystem.downloadAsync(
+    Asset.fromModule(require('./assets/data/data.db')).uri,
+    FileSystem.documentDirectory + 'SQLite/data.db'
+  )
+
+  return SQLite.openDatabase('data.db');
+}
 
 export default function App() {
+
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [hasError, setHasError] = useState(false)
+  const [errorMsg, setErrorMsg] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+
+    // Load data
+    openDatabase().then(db => {
+      console.log('open database')
+
+      db.transaction(tx => {
+        console.log('transaction')
+
+        tx.executeSql('select * from classes order by nm_nome', [], (_, { rows }) => {
+          let items = []
+
+          // simulate an slow load
+          setTimeout(() => {
+            console.log('delayed 3s')
+            for (let i = 0; i < rows.length; i++) {
+              items.push(rows.item(i))
+            }
+
+            setData(items)
+            setLoading(false)
+          }, 3000)
+
+        }, (sqlError) => {
+          console.log("SQL Error")
+          console.log(sqlError)
+          setLoading(false)
+          setHasError(true)
+          setErrorMsg(sqlError)
+        })
+      }, (txError) => {
+        console.log("Transaction Error")
+        console.log(txError)
+        setLoading(false)
+        setHasError(true)
+        setErrorMsg(txError)
+      })
+    }).catch( (openDbError) => {
+        console.log("Database Error")
+        setLoading(false)
+        setHasError(true)
+        setErrorMsg(openDbError)
+    })
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text key={'title'}>SQLite test app</Text>
+
+      {loading ?
+        <Text key={'loading'}>Loading...</Text>
+        : hasError ?
+          <Text key={'error'}>Error occured: {errorMsg}</Text>
+          : (data.map(row => <Text key={row['id']}>{row['nm_nome']}</Text>))
+      }
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +94,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
